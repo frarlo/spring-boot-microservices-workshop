02df4f8abd0c52db7cc13dcc3464a683ef01be05
frarlo8 <farmentalo8@gmail.com>
2024-06-27 20:04:49 +0200

    02. Added Movie Catalog API

---
+++ movie-catalog-service/src/main/java/com/io/javabrains/movie_catalog_service/resources/MovieCatalogResource.java
@@ -0,0 +1,26 @@
+package com.io.javabrains.movie_catalog_service.resources;
+
+import com.io.javabrains.movie_catalog_service.models.CatalogItem;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+
+        return Collections.singletonList(
+                new CatalogItem("Interstellar",
+                        "A movie about the space...",
+                        5)
+        );
+
+    }
+}
