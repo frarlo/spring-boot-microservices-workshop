6e0067e0c369bf0f2afdc4d9a942761cb2aa2d9e
frarlo8 <farmentalo8@gmail.com>
2024-06-28 10:36:10 +0200

    05/06/07 Incrementals
    05. Added Ratings Data Service API.
    06. Hardcoded ratings in catalog service.
    07. Used RestTemplate to call Movie Info API.

--- movie-catalog-service/src/main/java/com/io/javabrains/movie_catalog_service/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/io/javabrains/movie_catalog_service/resources/MovieCatalogResource.java
@@ -1,26 +1,42 @@
 package com.io.javabrains.movie_catalog_service.resources;
 
 import com.io.javabrains.movie_catalog_service.models.CatalogItem;
+import com.io.javabrains.movie_catalog_service.models.Movie;
+import com.io.javabrains.movie_catalog_service.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 
-        return Collections.singletonList(
-                new CatalogItem("Interstellar",
-                        "A movie about the space...",
-                        5)
+        // Get all rated movie IDs - Hardcoded for now:
+        List<Rating> ratingsList = Arrays.asList(
+                new Rating ("1234", 4),
+                new Rating ("5678", 3)
         );
 
+        // For each movie ID, call the movie info service and get its details and put them all together:
+        return ratingsList.stream().map(rating -> {
+
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getName(), "Description", rating.getRating());
+
+        }).collect(Collectors.toList());
     }
 }
